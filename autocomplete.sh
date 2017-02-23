0d9458a9864e0ba52e4d81d4cb87fb99ddc917a1
William Floyd <william.png2000@gmail.com>
2017-02-23 19:10:30 +1000

    Added bash autocomplete for long options and power of 2 sizes.

---
+++ autocomplete.sh
@@ -0,0 +1,25 @@
+_smelt () {
+    local cur prev words cword
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="--help --verbose --install --mobile --slow --time --debug --lengthy --force --quiet --warn --compress --force-xml"
+
+
+    case "${cur}" in
+        -*)
+            COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+            return 0
+            ;;
+        [0-9]*)
+            local sizes=$(for num in $(seq 5 12); do echo "2^${num}" | bc; done)
+            COMPREPLY=($(compgen -W "${sizes}" -- ${cur}))
+            return 0
+            ;;
+        *)
+        ;;
+    esac
+
+} &&
+
+complete -F _smelt smelt
