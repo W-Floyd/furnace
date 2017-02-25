bc84632a55ffa04afe467213aabeb472b9cf23f5
William Floyd <william.png2000@gmail.com>
2017-02-25 20:50:09 +1000

    Added comments to example config script for better explaination.

--- config.sh
+++ config.sh
@@ -4,10 +4,22 @@ compgen -A variable > /tmp/tmpvars
 # Start customizing from here
 ################################################################
 
+# Name of the pack, defaults to folder name if missing
 __name='Pack'
+
+# Temporary directory to use. Defaults to /tmp/texpack${__pid}
+# if missing
 __tmp_dir="$(pwd)/tmp/${__pid}"
+
+# Catalogue file to use. Defaults to catalogue.xml if missing
 __catalogue='./catalogue.xml'
+
+# Script to use for mobile conversion. No default currently,
+# will disable mobile conversion if missing
 __smelt_make_mobile_bin='./convert_to_mobile.sh'
+
+# Whether or not to render in quick mode (if applicable).
+# Defaults to quick mode if missing
 __quick='1'
 
 ################################################################
