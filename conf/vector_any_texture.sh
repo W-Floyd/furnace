2c4875a095875b66c2f0298aa0c2c17524ad558b
William Floyd <william.png2000@gmail.com>
2017-03-16 21:19:32 +1000

    vector_any_texture makes the bg according to the size the rendered image, not the image scale.

--- conf/vector_any_texture.sh
+++ conf/vector_any_texture.sh
@@ -6,7 +6,7 @@
 __vector_render "${1}" "${2}.svg"
 
 if ! [ -z "${3}" ]; then
-    convert -size "${1}x${1}" "canvas:${3}" "${2}_.png"
+    convert -size "$(identify -format "%w" "${2}.png")x$(identify -format "%h" "${2}.png")" "canvas:${3}" "${2}_.png"
     __overlay "${2}_.png" "${2}.png" "${2}__.png"
     rm "${2}_.png"
     mv "${2}__.png" "${2}.png"
