dca7d3f04799779209b28459a28041a9dfae04be
William Floyd <william.png2000@gmail.com>
2017-05-26 21:47:47 +1000

    composite: Makes composition functions verify themselves.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- functions/image/composite.sh
+++ functions/image/composite.sh
@@ -4,6 +4,86 @@
 
 ################################################################################
 #
+# __composite_common <METHOD> <BASE.png> <OVERLAY.png> <OUTPUT.png>
+#
+# Composite Images
+# Composites specified images, using specified blending method (eg. 'Multiply').
+#
+################################################################################
+
+__composite_common () {
+composite $(__imagemagick_define) -compose "${1}" "${3}" "${2}" "${4}"
+}
+
+__routine__image_overlay__composite () {
+__routine__image_src_over__composite ${@}
+}
+
+__routine__image_multiply__composite () {
+__composite_common Multiply $@
+}
+
+__routine__image_screen__composite () {
+__composite_common Screen $@
+}
+
+__routine__image_src_over__composite () {
+__composite_common src-over $@
+}
+
+__routine__image_dst_over__composite () {
+__composite_common dst-over $@
+}
+
+__routine__image_src_in__composite () {
+__composite_common src-in $@
+}
+
+__routine__image_dst_in__composite () {
+__composite_common dst-in $@
+}
+
+__routine__image_src_out__composite () {
+__composite_common src-out $@
+}
+
+__routine__image_dst_out__composite () {
+__composite_common dst-out $@
+}
+
+__routine__image_src_atop__composite () {
+__composite_common src-atop $@
+}
+
+__routine__image_dst_atop__composite () {
+__composite_common dst-atop $@
+}
+
+__routine__image_xor__composite () {
+__composite_common xor $@
+}
+
+################################################################################
+#
+# __composite_manager <METHOD> <BASE.png> <OVERLAY.png> <OUTPUT.png>
+#
+# Composite Manager
+# Manages composite routine running.
+#
+################################################################################
+
+__composite_manager () {
+local __prefix="$(sed 's/-/_/g' <<< "image_${1}")"
+
+__choose_function -e -d "compositing (${1})" -p 'composite' "${__prefix}"
+
+shift
+
+__run_routine "${__prefix}" "${1}" "${2}" "${3}"
+}
+
+################################################################################
+#
 # __overlay <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
 # Overlay Images
@@ -13,7 +93,9 @@
 ################################################################################
 
 __overlay () {
-__clip_src_over ${@}
+
+__composite_manager overlay $@
+
 }
 
 ################################################################################
@@ -26,7 +108,9 @@ __clip_src_over ${@}
 ################################################################################
 
 __multiply () {
-composite $(__imagemagick_define) -compose Multiply "${2}" "${1}" "${3}"
+
+__composite_manager multiply $@
+
 }
 
 ################################################################################
@@ -39,122 +123,142 @@ composite $(__imagemagick_define) -compose Multiply "${2}" "${1}" "${3}"
 ################################################################################
 
 __screen () {
-composite $(__imagemagick_define) -compose Screen "${2}" "${1}" "${3}"
+
+__composite_manager screen $@
+
 }
 
 ################################################################################
 #
 # __clip_src_over <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with src-over alpha blending
 #
 ################################################################################
 
 __clip_src_over () {
-composite $(__imagemagick_define) -compose src-over "${2}" "${1}" "${3}"
+
+__composite_manager src-over $@
+
 }
 
 ################################################################################
 #
 # __clip_dst_over <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with dst-over alpha blending
 #
 ################################################################################
 
 __clip_dst_over () {
-composite $(__imagemagick_define) -compose dst-over "${2}" "${1}" "${3}"
+
+__composite_manager dst-over $@
+
 }
 
 ################################################################################
 #
 # __clip_src_in <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with src-in alpha blending
 #
 ################################################################################
 
 __clip_src_in () {
-composite $(__imagemagick_define) -compose src-in "${2}" "${1}" "${3}"
+
+__composite_manager src-in $@
+
 }
 
 ################################################################################
 #
 # __clip_dst_in <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with dst-in alpha blending
 #
 ################################################################################
 
 __clip_dst_in () {
-composite $(__imagemagick_define) -compose dst-in "${2}" "${1}" "${3}"
+
+__composite_manager dst-in $@
+
 }
 
 ################################################################################
 #
 # __clip_src_out <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with src-out alpha blending
 #
 ################################################################################
 
 __clip_src_out () {
-composite $(__imagemagick_define) -compose src-out "${2}" "${1}" "${3}"
+
+__composite_manager src-out $@
+
 }
 
 ################################################################################
 #
 # __clip_dst_out <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with dst-out alpha blending
 #
 ################################################################################
 
 __clip_dst_out () {
-composite $(__imagemagick_define) -compose dst-out "${2}" "${1}" "${3}"
+
+__composite_manager dst-out $@
+
 }
 
 ################################################################################
 #
 # __clip_src_atop <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with src-atop alpha blending
 #
 ################################################################################
 
 __clip_src_atop () {
-composite $(__imagemagick_define) -compose src-atop "${2}" "${1}" "${3}"
+
+__composite_manager src-atop $@
+
 }
 
 ################################################################################
 #
 # __clip_dst_atop <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with dst-atop alpha blending
 #
 ################################################################################
 
 __clip_dst_atop () {
-composite $(__imagemagick_define) -compose dst-atop "${2}" "${1}" "${3}"
+
+__composite_manager dst-atop $@
+
 }
 
 ################################################################################
 #
 # __clip_xor <BASE.png> <OVERLAY.png> <OUTPUT.png>
 #
-# Screen Images
+# Composite Images
 # Composites specified images, with xor alpha blending
 #
 ################################################################################
 
 __clip_xor () {
-composite $(__imagemagick_define) -compose xor "${2}" "${1}" "${3}"
+
+__composite_manager xor $@
+
 }
