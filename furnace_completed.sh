2c97729d53f116509bbea9a74947eef2a5040a04
William Floyd <william.png2000@gmail.com>
2017-05-28 00:12:42 +1000

    completed: Indents for loop.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- furnace_completed.sh
+++ furnace_completed.sh
@@ -8,21 +8,21 @@ __clean_pack < "${1}" > "${__catalogue}"
 
 for __range in $(__get_range "${__catalogue}" ITEM); do
 
-__read_range "${__catalogue}" "${__range}" > "/tmp/readrangetmp"
+    __read_range "${__catalogue}" "${__range}" > "/tmp/readrangetmp"
 
-touch "/tmp/readrangetmp" "/tmp/commontmp" "/tmp/nametmp"
+    touch "/tmp/readrangetmp" "/tmp/commontmp" "/tmp/nametmp"
 
-if [ "$(__get_value "/tmp/readrangetmp" KEEP)" = 'YES' ]; then
+    if [ "$(__get_value "/tmp/readrangetmp" KEEP)" = 'YES' ]; then
 
-	__common="$(__get_value "/tmp/readrangetmp" COMMON)"
+	    __common="$(__get_value "/tmp/readrangetmp" COMMON)"
 
-	if ! [ -z "${__common}" ]; then
-	    echo "${__common}" >> "/tmp/commontmp"
-	else
-	    __get_value "/tmp/readrangetmp" NAME >> "/tmp/nametmp"
-	    __un_named="$(bc <<< "${__un_named}+1")"
-	fi
-fi
+	    if ! [ -z "${__common}" ]; then
+	        echo "${__common}" >> "/tmp/commontmp"
+	    else
+	        __get_value "/tmp/readrangetmp" NAME >> "/tmp/nametmp"
+	        __un_named="$(bc <<< "${__un_named}+1")"
+	    fi
+    fi
 
 done
 
